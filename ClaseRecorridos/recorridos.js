817a6acbc349a040e6ff2762e62bb552b25a3c7e
JuanOssa1 <juan.ossa@hotmail.com>
2022-02-04 15:45:55 -0500

    Curso finalizado

---
+++ ClaseRecorridos/recorridos.js
@@ -0,0 +1,9 @@
+var articulos = [
+    {nombre: "bici", costo: 3000},
+    {nombre: "casa", costo: 333},
+    {nombre: "perro", costo: 222},
+    {nombre: "camion", costo: 30000},
+    {nombre: "laptop", costo: 20000},
+    {nombre: "teclado", costo: 500},
+    {nombre: "audifonos", costo: 300}
+]
